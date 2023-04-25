d84559bcf4bca7e01e6b487ef4fe657e7b43f320
ccastano46 <castanocamilo522@gmail.com>
2023-04-25 03:31:42 -0500

    Hecho para comenzar ciclo4

---
+++ Conecta4/src/presentation/BoardElement.java
@@ -0,0 +1,48 @@
+package presentation;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import static javax.swing.SwingConstants.CENTER;
+import javax.swing.border.AbstractBorder;
+/**
+ * Clase que crea Label circular, basado en el siguiente enlace https://www.jc-mouse.net/java/jlabel-circular
+ * @web htpp://www.jc-mouse.net
+ * @author Mouse
+ */
+public class BoardElement extends JLabel {
+
+   private AbstractBorder circleBorder = new CircleBorder();       
+   private int lineBorder=1; 
+   private Color lineColor= Color.BLACK;
+
+    /** Constructor */
+     public BoardElement()
+     {
+        Dimension d = new Dimension(10,10);
+        setSize(d);
+        setOpaque(true);
+        setBorder(circleBorder); 
+     }
+
+    //Color de borde
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(Color color) {
+        circleBorder = new CircleBorder(color, lineBorder);
+        lineColor = color;
+        setBorder(circleBorder); 
+    }
+
+    //Grosor de borde
+    public int getLineBorder() {
+        return lineBorder;        
+    }
+
+    public void setLineBorder(int lineBorder) {
+        circleBorder = new CircleBorder(lineColor, lineBorder);
+        this.lineBorder = lineBorder;        
+        setBorder(circleBorder); 
+    }
+}
--- Conecta4/src/presentation/Conecta4GUI.java
+++ Conecta4/src/presentation/Conecta4GUI.java
@@ -2,12 +2,19 @@ package presentation;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.*;
 import java.awt.event.*;
 
 public class Conecta4GUI extends JFrame {
     private JMenuBar menuBar;
+    private JFileChooser selectorArchivos;
     private JMenuItem nuevo, abrir, salvar, salir;
+    private JPanel board;
+    private JLabel turno;
+    private JButton row1,row2,row3,row4,row5,row6;
 
     /**
      * Constructor de la interfaz grafica del juego Conecta4
@@ -21,7 +28,9 @@ public class Conecta4GUI extends JFrame {
     /**
      * Metodo que prepara los componentes dentro del Frame
      */
-    private void prepareElements() {
+    
+    
+     private void prepareElements() {
         setTitle("Conecta4");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize(screenSize.width / 2, screenSize.height / 2);
@@ -29,6 +38,9 @@ public class Conecta4GUI extends JFrame {
         this.setLocation((screenSize.width - frameSize.width) / 2,
                 (screenSize.height - frameSize.height) / 2);
         prepareElementsMenu();
+        selectorArchivos = new JFileChooser();
+        prepareElementsBoard();
+        
     }
 
     /**
@@ -52,6 +64,45 @@ public class Conecta4GUI extends JFrame {
     }
 
     /**
+     * Metodo que inicializa y organiza los componentes que hacen parte del tablero del juego
+     */
+    private void prepareElementsBoard(){
+        board = new JPanel();
+        board.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        board.setLayout(new GridLayout(6, 7, 15, 15));
+        for(int i = 0; i < 6*7; i++){
+            board.add(new BoardElement());
+        }
+        board.setBackground(new Color(70,165,162));
+        board.setOpaque(true);
+        turno = new JLabel("Es el turno del Jugador1", JLabel.CENTER);
+        JPanel botones = new JPanel(new GridLayout(6,1,5,5));
+        botones.setBorder(new CompoundBorder(new EmptyBorder(3,3,3,3), new TitledBorder("Filas")));
+        row1 = new JButton("Fila1");
+        botones.add(row1);
+        row2 = new JButton("Fila2");
+        botones.add(row2);
+        row3 = new JButton("Fila3");
+        botones.add(row3);
+        row4 = new JButton("Fila4");
+        botones.add(row4);
+        row5 = new JButton("Fila5");
+        botones.add(row5);
+        row6 = new JButton("Fila6");
+        botones.add(row6);
+        setContentPane(new JPanel());
+        getContentPane().setLayout(new BorderLayout(10,10));
+        ((JPanel)getContentPane()).setBorder(BorderFactory.createEmptyBorder(5,5,15,15));
+        getContentPane().add(board, BorderLayout.CENTER);
+        getContentPane().add(turno, BorderLayout.NORTH);
+        getContentPane().add(botones, BorderLayout.WEST);
+    }
+
+    private void refresh(){
+
+    }
+
+    /**
      * Metodo que prepara las acciones que se van a realizar.
      */
     private void prepareActions() {
@@ -64,18 +115,23 @@ public class Conecta4GUI extends JFrame {
                     setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
             }
         });
-
         prepareActionsMenu();
     }
-
     private void prepareActionsMenu() {
         salir.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 int siNo = JOptionPane.showConfirmDialog(Conecta4GUI.this, "¿Esta seguro de terminar el juego?");
-                if (siNo == 0)
-                    Conecta4GUI.this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                else
-                    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                if (siNo == 0) System.exit(0);
+            }
+        });
+        abrir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                selectorArchivos.showOpenDialog(Conecta4GUI.this);
+            }
+        });
+        salvar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                selectorArchivos.showSaveDialog(Conecta4GUI.this);
             }
         });
     }
